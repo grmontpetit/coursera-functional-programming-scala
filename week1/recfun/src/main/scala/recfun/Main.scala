32668c372aa9753dcc51c61813e987b81f6895ce
Gabriel Robitaille-Montpetit <grmontpetit@gmail.com>
2017-07-17 13:11:28 -0400

    Added a missing space

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -17,7 +17,7 @@ object Main {
     def pascal(c: Int, r: Int): Int = {
       if (c == 0 || r == 1 || c == r) {
         1
-      } else if (c == 1 || c == r -1) {
+      } else if (c == 1 || c == r - 1) {
         r
       } else {
         pascal(c - 1, r - 1) + pascal(c, r - 1)
