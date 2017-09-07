3f2e441b2e502b2aec891023e61a1124a18912d2
sniggel <grmontpetit@gmail.com>
2017-09-07 08:02:35 -0400

    Added week5 list implementation

--- week5/src/main/scala/week5/Main.scala
+++ week5/src/main/scala/week5/Main.scala
@@ -1,3 +1,21 @@
 object Main extends App {
   println("This is week 5")
+
+  val myList = List('a', 'b', 'c', 'd')
+
+  println(last(myList))
+  println(init(myList))
+
+  def last[T](xs: List[T]): T = xs match {
+    case List()  => throw new Error("last on empty list")
+    case List(x) => x
+    case _ :: ys => last(ys)
+  }
+
+  def init[T](xs: List[T]): List[T] = xs match {
+    case List()  => throw new Error("init on empty list")
+    case List(x) => List()
+    case y :: ys => y :: init(ys)
+  }
+
 }
