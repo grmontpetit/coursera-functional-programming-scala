560096d29882d62b64a26b3dd80cf1e1b9b4c2ba
sniggel <grmontpetit@gmail.com>
2017-09-10 23:20:26 -0400

    Added lecture 5.1 exercices

--- week5/src/main/scala/week5/Main.scala
+++ week5/src/main/scala/week5/Main.scala
@@ -5,6 +5,8 @@ object Main extends App {
 
   println(last(myList))
   println(init(myList))
+  println(reverse(myList))
+  println(removeAt(myList, 2))
 
   def last[T](xs: List[T]): T = xs match {
     case List()  => throw new Error("last on empty list")
@@ -18,4 +20,21 @@ object Main extends App {
     case y :: ys => y :: init(ys)
   }
 
+  def concat[T](xs: List[T], ys: List[T]): List[T] = xs match {
+    case List()  => ys
+    case z :: zs => z :: concat(zs, ys)
+  }
+
+  // n * n complexity
+  def reverse[T](xs: List[T]): List[T] = xs match {
+    case List()  => xs
+    case y :: ys => reverse(ys) :+ y
+  }
+
+  def removeAt[T](xs: List[T], n: Int): List[T] = xs match {
+    case List()       => xs
+    case head :: tail =>
+      if (n == 0) tail
+      else head :: removeAt(tail, n - 1)
+  }
 }
